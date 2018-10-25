4a3da920b50588ac66141b44c13e68be0afdf63a
Dave Sole <David.Sole@minfos.com.au>
2018-10-25 17:14:49 +1100

    Inline the checkout to hide the exit code

--- build/ci/merge-check.sh
+++ build/ci/merge-check.sh
@@ -7,15 +7,14 @@ if [ -z $(git config --global user.name) ]; then
   git config --global user.email "unknown@example.org"
 fi
 
-# Merge the source branch into master and verify there are no merge-conflicts
-git checkout master
-
-if [ $? != 0 ]; then
+# Merge the source branch into master
+if ! git checkout master; then
   git remote set-branches --add origin master
   git fetch origin master
   git checkout master
 fi
 
+# Verify there are no merge-conflicts
 git merge --no-ff --no-commit $BITBUCKET_COMMIT
 if [[ -n $(git ls-files -u) ]]; then exit 1; fi
 

c978e47004f13213f652eba3278516ce5c240e6e
Dave Sole <David.Sole@minfos.com.au>
2018-10-26 11:41:32 +1100

    Fix the merge-check pipeline when running it on a branch by authenticating against BitBucket and adding 'master' as an available remote branch

---
+++ build/ci/get-access-token.js
@@ -0,0 +1,39 @@
+const { request } = require("https");
+var querystring = require('querystring');
+const { parse } = require("url");
+
+var post_data = querystring.stringify({
+  grant_type: "refresh_token",
+  refresh_token: (process.argv[3] || process.env.BITBUCKET_REFRESH_TOKEN)
+});
+
+const options = parse("https://bitbucket.org/site/oauth2/access_token");
+
+options.headers = {
+  "Accept": "*/*",
+  "Authorization": "Basic " + (process.argv[2] || process.env.BITBUCKET_AUTHORIZATION),
+  "Content-Type": "application/x-www-form-urlencoded",
+  'Content-Length': Buffer.byteLength(post_data)
+};
+
+options.method = "POST";
+
+const req = request(options, (res) => {
+  let body = "";
+
+  res.on("data", (chunk) => body += chunk.toString());
+
+  res.on("end", () => {
+    try {
+      const reply = JSON.parse(body);
+      if (reply.access_token) {
+        console.log(reply.access_token);
+      }
+    } catch (err) {
+      // do nothing
+    }
+  });
+});
+
+req.write(post_data);
+req.end();
--- build/ci/merge-check.sh
+++ build/ci/merge-check.sh
@@ -7,8 +7,21 @@ if [ -z $(git config --global user.name) ]; then
   git config --global user.email "unknown@example.org"
 fi
 
-# Merge the source branch into master and verify there are no merge-conflicts
-git checkout master
+
+# Merge the source branch into master
+if ! git checkout master; then
+
+  set +x
+  REPOSITORY_OAUTH_ACCESS_TOKEN=`node build/ci/get-access-token.js`
+  git remote set-url origin https://x-token-auth:$REPOSITORY_OAUTH_ACCESS_TOKEN@bitbucket.org/minfos/ref-boilerplate-core.git
+  set -x
+
+  git remote set-branches --add origin master
+  git fetch origin master
+  git checkout master
+fi
+
+# Verify there are no merge-conflicts
 git merge --no-ff --no-commit $BITBUCKET_COMMIT
 if [[ -n $(git ls-files -u) ]]; then exit 1; fi
 

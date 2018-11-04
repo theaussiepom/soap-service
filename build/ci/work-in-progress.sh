f1edb1e6ceb084ac5a157b40a9f0777ba2371f72
Dave Sole <David.Sole@minfos.com.au>
2018-11-05 10:47:44 +1100

    Convert test results before exiting after a failed spec-features test

--- build/ci/work-in-progress.sh
+++ build/ci/work-in-progress.sh
@@ -4,9 +4,13 @@ set -x
 npm run build
 mkdir test-results
 npm install cucumber-junit mocha-junit-reporter
+
 npm run test:unit:coverage -- --reporter mocha-junit-reporter --reporter-options mochaFile=./test-results/unit.xml
+
 npm run test:spec:features:coverage -- --tags 'not @wip' --format progress --format json:test-results/spec-features.json
+WIP_SPECS_PASS=$?
 cat test-results/spec-features.json node_modules/.bin/cucumber-junit > test-results/spec-features.xml
+if [[ $WIP_SPECS_PASS != 0 ]]; then exit $WIP_SPECS_PASS; fi
 
 PKG_VERSION=`node -p "require('./package.json').version"`
 VERSION=`node_modules/.bin/semver $PKG_VERSION -i prerelease --preid dev | sed "s/[0-9]\+$/$BITBUCKET_BUILD_NUMBER/"`

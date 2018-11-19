ca8636ef41515ca34504b21390b78ae321749699
Dave Sole <David.Sole@minfos.com.au>
2018-11-20 09:40:40 +1100

    fix(): Only tag the WIP package if a branch name is available

--- build/ci/work-in-progress.sh
+++ build/ci/work-in-progress.sh
@@ -18,11 +18,15 @@ PKG_VERSION=`node -p "require('./package.json').version"`
 VERSION=`node_modules/.bin/semver $PKG_VERSION -i prerelease --preid dev | sed "s/[0-9]\+$/$BITBUCKET_BUILD_NUMBER/"`
 npm version $VERSION --git-tag-version false
 
-TAG=`echo $BITBUCKET_BRANCH | tr '[:upper:]' '[:lower:]' | sed -e 's_/_-_g' -e 's/[^a-zA-Z0-9-]//g'`
+if [ -n "$BITBUCKET_BRANCH" ];  then
+  TAG=`echo $BITBUCKET_BRANCH | tr '[:upper:]' '[:lower:]' | sed -e 's_/_-_g' -e 's/[^a-zA-Z0-9-]//g'`
+  npm publish --tag $TAG
+  echo "Published dev package with tag:" $TAG
+else
+  npm publish
+  echo "Published dev package with no tag"
+fi
 
-npm publish --tag $TAG
 npm access grant read-only minfos:read-only
-
-echo "Published dev package with tag:" $TAG
 
 set +x
